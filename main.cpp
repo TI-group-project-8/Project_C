cfeaf5841e437818a1f78bd51d1e3f499e0de3b2
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2021-03-30 14:30:11 +0200

    Bug fix

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ void startrps(){
         else if((inputp1==schaar)&&(inputp2==papier)){
             cout<<"Speler 1 wint!\n";
             p1score++;}
-        else if((inputp1==schaar)&&(inputp2==schaar))
+        else if((inputp1==schaar)&&(inputp2==schaar)){
             cout<<"Gelijkspel\n";}
 
         else{
