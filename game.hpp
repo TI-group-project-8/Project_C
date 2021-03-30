cbd30af7dbe6250f9ef28e780fde1705a1aebabc
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2021-03-30 14:14:58 +0200

    Header voor LCD toegevoegd

--- game.hpp
+++ game.hpp
@@ -6,3 +6,12 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+int lcdRS = 25;
+int lcdE = 24;
+int lcdD4 = 23;
+int lcdD5 = 22;
+int lcdD6 = 21;
+int lcdD7 = 14;
+
+void schrijfNaarLCD(int lcd, string tekst, int xPositie, int yPositie, int tijd);
